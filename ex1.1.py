9c675cfdc64b8f2f685866b26eaa3b7ce8a41340
Renata Machado Barreto Braga <97547658+renata-machado@users.noreply.github.com>
2024-04-20 12:07:50 -0300

    Delete ex1.1.py

--- ex1.1.py
+++
@@ -1,7 +0,0 @@
-"""Exercício 1.1 Armazene seu nome e idade em variáveis separadas e imprima uma saída formatada
-com elas."""
-
-nome = "Renata"
-idade = 18
-
-print(f"Meu nome é {nome} e tenho {idade} anos.")
