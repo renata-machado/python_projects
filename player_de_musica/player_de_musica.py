00521fff33255355bd6432939f1b8981eb9dde25
Renata <renatamachadobarreto@gmail.com>
2024-09-01 11:50:01 -0300

    agora só falta deixar bonito

--- player_de_musica/player_de_musica.py
+++ player_de_musica/player_de_musica.py
@@ -2,12 +2,14 @@ from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
 import os
+from pygame import mixer
 
 diretorio = ""
 pasta_default = "player_de_musica/musicas" 
 lista_musicas = []
 indice_musica = 0
 musica_atual = ""
+tocando = False
 
 def selecionar_arquivo():
     global diretorio, lista_musicas, indice_musica, musica_atual
@@ -17,7 +19,7 @@ def selecionar_arquivo():
     )
 
     nome_do_arquivo = fd.askopenfilename(
-        title='Open a file',
+        title='Selecione um arquivo',
         initialdir=os.path.normpath(pasta_default),
         filetypes=filetypes
     )
@@ -27,7 +29,7 @@ def selecionar_arquivo():
         musica_atual = os.path.basename(nome_do_arquivo)
 
         lista_musicas = [arquivo for arquivo in os.listdir(diretorio) if arquivo.endswith('.mp3')]
-        lista_musicas = [os.path.join(diretorio, arquivo) for arquivo in lista_musicas] #caminho completo da musica
+        lista_musicas = [os.path.join(diretorio, arquivo) for arquivo in lista_musicas] # caminho completo da musica
         
         try:
             indice_musica = lista_musicas.index(nome_do_arquivo)
@@ -35,7 +37,7 @@ def selecionar_arquivo():
             indice_musica = 0  
 
         atualizar_label()
-
+        tocar_musica()
 
 def atualizar_label():
     texto.config(text=musica_atual)
@@ -47,7 +49,7 @@ def prox_musica():
         indice_musica = (indice_musica + 1) % len(lista_musicas)
         musica_atual = os.path.basename(lista_musicas[indice_musica])
         atualizar_label()
-
+        tocar_musica()
 
 def musica_anterior():
     global indice_musica, musica_atual, lista_musicas
@@ -56,9 +58,32 @@ def musica_anterior():
         indice_musica = (indice_musica - 1) % len(lista_musicas)
         musica_atual = os.path.basename(lista_musicas[indice_musica])
         atualizar_label()
+        tocar_musica()
 
+def tocar_musica():
+    global tocando, musica_atual, lista_musicas
+
+    if lista_musicas:
+        mixer.music.load(lista_musicas[indice_musica])
+        mixer.music.play()
+        tocando = True
+        botao_play.config(text="||")
+
+def pausar_musica():
+    global tocando
+
+    if tocando:
+        mixer.music.pause()
+        tocando = False
+        botao_play.config(text="|>")
+
+    else:
+        mixer.music.unpause()
+        tocando = True
+        botao_play.config(text="||")
 
 # Configuracoes 
+mixer.init()
 janela = Tk()
 janela.geometry("300x200")
 janela.title("Player de Música")
@@ -71,7 +96,7 @@ frame_botoes.pack(side=BOTTOM, pady=15)
 botao_anterior = Button(frame_botoes, text="<", command=musica_anterior)
 botao_anterior.pack(side=LEFT, padx=5)
 
-botao_play = Button(frame_botoes, text="||", command=lambda: None)
+botao_play = Button(frame_botoes, text="|>", command=pausar_musica)
 botao_play.pack(side=LEFT, padx=5)
 
 botao_proxima = Button(frame_botoes, text=">", command=prox_musica)
@@ -80,8 +105,8 @@ botao_proxima.pack(side=LEFT, padx=5)
 botao_selecionar_musica = ttk.Button(frame_botoes, text="Selecionar Música", command=selecionar_arquivo)
 botao_selecionar_musica.pack(side=LEFT, padx=5)
 
-#Exibe a música
-texto = Label(janela, text="nenhuma música selecionada")
+# Exibe a música
+texto = Label(janela, text="Nenhuma música selecionada")
 texto.pack(side=BOTTOM, pady=5)
 
 janela.mainloop()
